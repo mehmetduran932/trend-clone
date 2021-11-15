edea9f54fea93f33475244d32d663e07c1b407ee
mehmetduran932 <48024660+mehmetduran932@users.noreply.github.com>
2021-11-15 22:56:47 +0300

    sepet özeti

---
+++ src/components/CardSummary.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Box, Flex, Spacer, Divider } from "@chakra-ui/react";
+import { connect } from "react-redux";
+
+
+function CardSummary({ cart }) {
+  const[count,setCount]=useState(0);
+
+  useEffect(() => {
+   
+    console.log("p",count)
+
+    totalPrice();
+  }, [count]);
+
+  async function totalPrice(count) {
+    cart.map((p)=>(
+        setCount(count+=p.price)
+    ))
+  }
+  return (
+    <div>
+      <Box
+        width="300px"
+        height="500px"
+        border="groove"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <h4>Sepet Özeti</h4>
+        <Divider w="300px" orientation="horizontal" />
+        <Flex>
+          <Box>
+            <h1>Ürün Toplamı {count} TL</h1>
+            <h1>Kargo Toplam </h1>
+            <Divider w="300px" orientation="horizontal" />
+            <h1>Sepet Toplamı </h1>
+          </Box>
+
+          <Spacer />
+          <Box p="4" bg="white.400"></Box>
+        </Flex>
+      </Box>
+    </div>
+  );
+}
+function mapStateToProps(state) {
+    return {
+      cart: state.cart,
+    };
+  }
+export default connect(mapStateToProps)(CardSummary)
--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -1,44 +1,56 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import CartEmptyError from "./CartEmptyError";
+import CardSummary from "./CardSummary";
+import { Box, Flex } from "@chakra-ui/react";
 
 function CartDetail({ cart }) {
-    useEffect(() => {
-        console.log("sepet:",cart)
-    }, [cart]);
-    if(cart.length===0){
-        return(
-            <div><CartEmptyError/></div>
-        )
-    }
+  useEffect(() => {
+    console.log("sepet:", cart);
+  }, [cart]);
+  if (cart.length === 0) {
+    return (
+      <div>
+        <CartEmptyError />
+      </div>
+    );
+  }
   return (
     <div>
-       
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>#</Th>
-            <Th>Product Name</Th>
-            <Th>Unit Price</Th>
-            <Th>Quantity</Th>
+      <Flex color="black">
+        <Box flex="1" bg="white">
+          <h1>Box 1</h1>
+        </Box>
+        <Box flex="3" bg="white">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>#</Th>
+                <Th>Product Name</Th>
+                <Th>Unit Price</Th>
+                <Th>Quantity</Th>
 
-            <Th></Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {cart.map((cartItem) => (
-            <Tr>
-              <Th scope="row">{cartItem.id}</Th>
-              <Td>{cartItem.productName}</Td>
-              <Td>{cartItem.price}</Td>
-              <Td>{cartItem.quantity}</Td>
-              
+                <Th></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {cart.map((cartItem) => (
+                <Tr>
+                  <Th scope="row">{cartItem.id}</Th>
+                  <Td>{cartItem.productName}</Td>
+                  <Td>{cartItem.price}</Td>
+                  <Td>{cartItem.quantity}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
 
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
+        <Box flex="1" bg="white">
+          <CardSummary/>
+        </Box>
+      </Flex>
     </div>
   );
 }
